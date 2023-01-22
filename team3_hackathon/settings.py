20bd3ec9b8c4a9e991669e702848177489b3360f
Tanise Carvalho <carvalhotanise@gmail.com>
2023-01-22 13:42:55 +0000

    Edit settings.py to remove DEBUG false

--- team3_hackathon/settings.py
+++ team3_hackathon/settings.py
@@ -12,8 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = False
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'team3-january-hackathon.onrender.com']
 
