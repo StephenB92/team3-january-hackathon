b5c19130c2651363c9a9c09c06fe52a7f3087452
Niki Tester <bloatedniknak@gmail.com>
2023-01-22 17:14:55 +0000

    add currency converter api

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("weather/", views.weather_api),
-    path("convert/", views.converter_api),
+    path("converter/", views.converter_api),
     path("get_currencies/", views.get_currencies),
 ]
