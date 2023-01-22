20a83cbe72919e3622097cddef39d360d1bd6e98
Niki Tester <bloatedniknak@gmail.com>
2023-01-22 17:23:54 +0000

    add bad request response if request is not post

--- api/views.py
+++ api/views.py
@@ -20,25 +20,27 @@ def converter_api(request):
     """
     A view to handle exchange rate api currency conversion
     """
-    body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-    from_query = body['from_query']
-    to_query = body['to_query']
-    amount_query = body['amount_query']
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        from_query = body['from_query']
+        to_query = body['to_query']
+        amount_query = body['amount_query']
 
-    url = ("https://api.apilayer.com/exchangerates_data/convert?"
-           f"to={to_query}&from={from_query}&amount={amount_query}")
+        url = ("https://api.apilayer.com/exchangerates_data/convert?"
+               f"to={to_query}&from={from_query}&amount={amount_query}")
 
-    payload = {}
-    headers = {
-        "apikey": settings.EXCHANGE_API_KEY
-    }
+        payload = {}
+        headers = {
+            "apikey": settings.EXCHANGE_API_KEY
+        }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-    status_code = response.status_code
-    data = json.loads(response.text)
-    return JsonResponse(data)
+        status_code = response.status_code
+        data = json.loads(response.text)
+        return JsonResponse(data)
+    return HttpResponse(status=400)
 
 
 def get_currencies(request):
